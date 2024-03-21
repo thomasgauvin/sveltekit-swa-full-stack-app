08b1318fc891b8c609023f0cd271f502349b4fe2
Thomas Gauvin <35609369+thomasgauvin@users.noreply.github.com>
2024-03-20 23:34:06 -0400

    showing env vars to make sure everything is working

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -2,6 +2,6 @@ import { env } from '$env/dynamic/private';
 
 export const load = ({ cookies, url }) => {
     console.log(cookies, url)
-    console.log(COSMOSDB_KEY, COSMOSDB_ENDPOINT)
-    return { info: `keys: ${env}` }
+    console.log(env)
+    return { info: env }
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
+<script>
+    export let data;
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div>
+    <p>Environment: {JSON.stringify(data)}</p>
+</div>
